1eb278d6caba9363746da648e6502bfa618776b9
Josh Wang <spjay1@gmail.com>
2021-01-21 20:10:59 +0800

    init folder

---
+++ src/Presentation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './style.module.scss';
+
+const HomePresentation = () => {
+  return (
+    <div className={style.wrap}>
+
+    </div>
+  )
+};
+
+export default HomePresentation;
